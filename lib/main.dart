5d65a0a464ef919e20088a1517ce7ca0d5043e56
Abdul-fatai <staticdev20046@gmail.com>
2020-01-24 03:48:42 +0100

    Audio config setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:speech_recognition/speech_recognition.dart';
 
 void main() {
-  runApp(MyApp);
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,5 +11,72 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: VoiceHome(),
     );
+  }
+}
+
+class VoiceHome extends StatefulWidget {
+  @override
+  _VoiceHomeState createState() => _VoiceHomeState();
+}
+
+class _VoiceHomeState extends State<VoiceHome> {
+  SpeechRecognition _speechRecognition;
+  bool _isAvailable = false;
+  bool _isListening = false;
+
+  String resultText = "";
+
+  @override
+  void initState() {
+    super.initState();
+    initSpeechRecognizer();
+  }
+
+  void initSpeechRecognizer() {
+    _speechRecognition = SpeechRecognition();
+    _speechRecognition.setAvailabilityHandler(
+      (bool result) => setState(() => _isAvailable = result),
+    );
+    _speechRecognition.setRecognitionStartedHandler(
+      () => setState(() => _isListening = true),
+    );
+
+    _speechRecognition.setRecognitionResultHandler(
+      (String speech) => setState(() => resultText = speech),
+    );
+
+    _speechRecognition.setRecognitionCompleteHandler(
+      () => setState(() => _isListening = false),
+    );
+
+    _speechRecognition
+        .activate()
+        .then((result) => setState(() => _isAvailable = result));
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Speech to Text'),
+        ),
+        body: Container(
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  FloatingActionButton(
+                    onPressed: () {},
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {},
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
