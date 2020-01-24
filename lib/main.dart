e2189b22241dc15a01af962b7c9651a55d598cbf
Abdul-fatai <staticdev20046@gmail.com>
2020-01-24 04:24:10 +0100

    UI updated

--- lib/main.dart
+++ lib/main.dart
@@ -57,23 +57,45 @@ class _VoiceHomeState extends State<VoiceHome> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Speech to Text'),
+          backgroundColor: Colors.purple,
         ),
         body: Container(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   FloatingActionButton(
                     onPressed: () {},
+                    child: Icon(Icons.cancel),
+                    mini: true,
+                    backgroundColor: Colors.deepOrange,
                   ),
                   FloatingActionButton(
                     onPressed: () {},
+                    child: Icon(Icons.mic),
+                    backgroundColor: Colors.pink,
                   ),
                   FloatingActionButton(
                     onPressed: () {},
+                    mini: true,
+                    child: Icon(Icons.stop),
+                    backgroundColor: Colors.deepPurple,
                   )
                 ],
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 0.6,
+                decoration: BoxDecoration(
+                  color: Colors.cyanAccent[100],
+                  borderRadius: BorderRadius.circular(6.0),
+                ),
+                padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 12.0),
+                child: Text(resultText),
               )
             ],
           ),
